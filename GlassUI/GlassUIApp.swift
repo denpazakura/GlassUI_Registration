007d86154a7323f5c3b1c5c76aecbeecd8864a41
Diana K <denpazakura@gmail.com>
2023-10-04 13:41:25 +0200

    Initial Commit

---
+++ GlassUI/GlassUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassUIApp.swift
+//  GlassUI
+//
+//  Created by Diana Komolova on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GlassUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
